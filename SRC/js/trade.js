9b9f1fcf4de4dc8321ae4c0d71796f4595f6a9ce
nakamura mari <202411ap01_nakamura@x-ide.sakura.ne.jp>
2025-02-04 15:42:00 +0900

    #34677　業者登録で登録ボタンを押下しても登録処理が行われない、確認メッセージが表示されない

--- SRC/js/trade.js
+++ SRC/js/trade.js
@@ -3,7 +3,7 @@
 //
 function fnTradeEditCheck() {
 	tmp = form.name.value;
-	if (tmp.length < 0) {
+	if (tmp.length == 0) {
 		alert('業者名を入力してください');
 		return;
 	}
@@ -12,7 +12,7 @@ function fnTradeEditCheck() {
 		return;
 	}
 
-	tmp = form.nameYomi.value;
+	tmp = form.nameFuri.value;
 	if (tmp.length > 100) {
 		alert('業者名（よみ）は100文字以内で入力してください');
 		return;
@@ -31,13 +31,13 @@ function fnTradeEditCheck() {
 	}
 
 	tmp = form.zip.value;
-	if (tmp.length > 0 && !tmp.match(/^\d{3}(\s*|-)\d{4}$/)) {
+	if (tmp.length > 0 && !tmp.match(/(^\d{3}(-)\d{4}$)|(\d{7})/)) {
 		alert("正しい郵便番号(***-**** 又は ******* )で\n入力してください");
 		return;
 	}
 
 	tmp = form.prefecture.value;
-	if (tmp.length >= 10) {
+	if (tmp.length > 10) {
 		alert('住所（都道府県）は10文字以内で入力してください');
 		return;
 	}
@@ -73,7 +73,7 @@ function fnTradeEditCheck() {
 	}
 
 	if (confirm('この内容で登録します。よろしいですか？')) {
-		form.act.value = 'tradeEditCompleta';
+		form.act.value = 'tradeEditComplete';
 		form.submit();
 	}
 }
@@ -83,6 +83,7 @@ function fnTradeEditCheck() {
 function fnTradeDeleteCheck(no) {
 	if (confirm('削除します。よろしいですか？')) {
 		form.tradeNo.value = no;
+		form.act.value = 'tradeDelete';
 		form.submit();
 	}
 }
