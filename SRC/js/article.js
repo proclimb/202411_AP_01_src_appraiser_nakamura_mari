f88aae3099d3bb3271cf1b3cdebcfa2926cf3199
nakamura mari <202411ap01_nakamura@x-ide.sakura.ne.jp>
2025-02-07 12:45:16 +0900

    #34861　物件管理の新規登録時、物件名を入力した状態でも「物件名を入力してください」のエラーメッセージが表示される。「この内容で登録します。よろしいですか？」の確認メッセージが表示されずに登録される

--- SRC/js/article.js
+++ SRC/js/article.js
@@ -3,22 +3,24 @@
 //
 function fnArticleEditCheck() {
 	tmp = form.article.value;
-	if (tmp) {
+	if (tmp.length == 0) {
 		alert('物件名を入力してください');
 		return;
 	}
 	if (isLength(100, "物件名", form.article)) { return; }
 
-	form.act.value = 'articleEditComplete';
-	form.submit();
-}
-
-
-
-function fnArticleDeleteCheck(no) {
-	if (confirm('削除します。よろしいですか？')) {
-		form.articleNo.value = no;
-		form.act.value = 'articleDelete';
+	if (confirm('この内容で登録します。よろしいですか？')) {
+		form.act.value = 'articleEditComplete';
 		form.submit();
+	}
+
+
+
+	function fnArticleDeleteCheck(no) {
+		if (confirm('削除します。よろしいですか？')) {
+			form.articleNo.value = no;
+			form.act.value = 'articleDelete';
+			form.submit();
+		}
 	}
 }
